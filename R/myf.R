1df0b0395a5eb5bd57bf80ffc6191abcd66063b6
Cassidy Krantz <cassidy@ou.edu>
2020-09-23 09:43:05 -0500

    Initial file list

---
+++ R/myf.R
@@ -0,0 +1,15 @@
+#' Square Function
+#'
+#' Takes a vector and returns a vector of squared components
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; mfy(x)
+#'
+myf <- function(x){
+  x^2
+}
